50e12bf796933c4889fa596cce4eebb5b73062c3
Freelancingonupwork <QKFKHu3g8LSYxSummauN>
2024-02-12 15:05:59 +0530

    Constants Added

---
+++ GymTemplateiOS/GymTemplateiOS/ProjectManager/App Utils/Constants/NetworkConstants.swift
@@ -0,0 +1,26 @@
+//
+//  NetworkConstants.swift
+//  GymTemplateiOS
+//
+//  Created by Potenza on 12/02/24.
+//
+
+import Foundation
+
+
+let baseUrl = "You base url..."
+
+/// This class contains the api keys
+class NetworkConstants {
+    
+    let kUserLogin =  baseUrl + "user login url"
+    let kForgotPassword =  baseUrl + "user login url"
+    let kResetPassword =  baseUrl + "user login url"
+    let kGetJobList =  baseUrl + "user login url"
+    let kGetJobDetail = baseUrl + "user login url"
+    
+    let kGetProfile = baseUrl + "user login url"
+    
+    
+    
+}
